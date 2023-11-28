222daf683a079b8451edc5b61c3634a99a981f31
Murilo Bezerra <78811958+Murilazuo@users.noreply.github.com>
2023-11-28 19:39:57 -0300

    Remove water level bar

--- Assets/Scripts/UI/Gameplay/WaterLevelBar.cs
+++ Assets/Scripts/UI/Gameplay/WaterLevelBar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,11 @@ public class WaterLevelBar : MonoBehaviour
 {
     [SerializeField] Image fill;
     [SerializeField] float timeToFill;
+    [SerializeField] CanvasGroup canvasGroup;
+    private void Start()
+    {
+        canvasGroup.alpha = 0f;
+    }
     void SetFill(int current, int max)
     {
         LeanTween.value(gameObject,SetFill, fill.fillAmount,(float)((float)current/(float)max) , timeToFill);
@@ -19,15 +25,23 @@ public class WaterLevelBar : MonoBehaviour
     {
         LeanTween.cancel(gameObject);
         fill.fillAmount = 0;
+        canvasGroup.alpha = 0;
+    }
+    private void StartConter()
+    {
+        canvasGroup.alpha = 1;
     }
     private void OnEnable()
     {
         DangerArea.OnSetDangerArea += SetFill;
         MatchManager.OnPlayAgain += ResetBar;
+        MatchManager.OnStarCounter += StartConter;
     }
     private void OnDisable()
     {
         DangerArea.OnSetDangerArea -= SetFill;
         MatchManager.OnPlayAgain -= ResetBar;
+        MatchManager.OnStarCounter -= StartConter;
+        
     }
 }
